7192528b91f89473107ed8e802e2be57ad9b6a45
jvrobinho <jvmonteirorj@gmail.com>
2017-06-18 16:48:33 -0300

    Falta achar e printar ponto de articulação; e falta printar componente fortemente conexa.

--- grafoed.c
+++ grafoed.c
@@ -1,175 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <grafo.h>
-typedef struct grafo{
-  TNO * prim;
-  int numCores;
-}TG;
-
-typedef struct no{
-  int id_no;
-  int cor;
-  TViz * prim_viz;
-  struc no * prox_no;
-}TNO;
-
-typedef struct viz{
-  int id_viz;
-  int custo;
-  struct viz * prox_viz;
-}TViz;
 
 
-void imprime (TG * g ){
-    TNO * p = g->prim;
-    while(p){
-        printf("%d \n", p->id_no);
-        TViz * v = p->prim_viz;
-        while(v){
-            printf("viz: %d \t custo: %d", v->id_viz, v->custo);
-            v=v->prox_viz;
-        }
-        p=p->prox_no;
-    }
-}
-void insereNo(TG * g, int elem){
-    TNO * aux = g->prim;
-    TNO * no = (TNO*)malloc(sizeof(TNO));
-    while(aux->prox_no){
-        aux = aux->prox_no;
-    } 
-    aux->prox_no = no;
-    no->id_no = elem;
-} 
-TNO * buscaNo(TG * g, int elem){
-    TNO * no = g->prim;
-    while(no){
-        if(elem== no->id_no){
-            return no;
-        }  
-        no = no->prox_no;
-    }
-    return null; 
-}
-void removeNO(TG * g, int elem){
-    TNO no = buscaNO(g, elem);
-    if(!no)
-        return;  
-    }  
-    while(no->prim_viz){
-        TNO * viz = no->prim_viz;
-        no->prim_viz = viz->prox_viz;
-        free(viz); 
-    }
-    free(no); 
-    TNO * aux = g->prim;
-
-    while(aux){
-        TViz * aux_viz = aux->prim_viz;
-        while(aux_viz){
-            if(aux_viz->id_viz == elem){
-                TViz * c = aux_viz;
-                aux_viz = aux_viz->prox_viz;
-                free(c); 
-            }  
-            aux = aux->prox_no;
-        }            
-    }
-     
-}
-
-
-TViz * buscaAresta(TG * g, int no1, int no2){
-    TNO * no = g->prim;
-    while(no){
-        if(no->id_no == no1){
-            TViz aresta = no->prim_viz;
-            while(aresta){
-                if(aresta->id_viz == no2){
-                    return aresta;   
-                }
-            }
-            return; 
-        }
-    }
-    return; 
-}
-void insereAresta(TG * g, int no1, int no2, int custo){
-    TViz aresta = buscaAresta(g,no1,no2);
-    if(aresta){
-    return;
-    }
-    TNO * n1 = buscaNo(g,no1);
-    TNO * n2 = buscaNo(g,no2);
-    if(n1 && n2){
-        TViz viz = n1->prim_viz;
-        while(viz->prox_viz){
-        viz= viz->prox_viz;
-        }
-        viz->prox_viz = (TViz*) malloc (sizeof(TViz));
-        viz = viz->prox_viz;
-        viz->custo = custo;
-        viz->id_viz = no2->id_no;
-    }
-}
-void removeAresta(TG * g, int no1, int no2){
-    TViz aresta = buscaAresta(g,no1,no2);
-    if(aresta){
-    TViz * r = aresta; 
-    aresta = aresta->prox_viz;
-    free(r);
-    }
-}
-void imprime (TG* g){
-    TNO * p = g-> prim;
-    while(p){
-        printf("%d \n", p->id_no);
-        TViz * v = p->prim_viz;
-        while(v){
-        printf("viz: %d \t custo %d",v->id_viz, v->custo);
-        v=v->prox_viz;"
-        }
-    }
-    p=p->prox_no;
-        
-}
-void libera (TG * g){
-    TNO * no = g->prim;
-    while(no){
-        TViz aresta = no->prim_viz;
-        while(aresta){
-            TViz aux = aresta; 
-            aresta = aresta->prox_viz;
-            free(aux); 
-        }
-        g->prim = no->prox_no;
-        TNO aux_no = no;
-        no = no->prox_no;
-        free(aux_no);
-    }
-}
-
-
-
-void fortementeConexa(TG * g){
-    TNO * no1 = g->prim;
-    while(no1){
-        TNO * no2 = g->prim;
-        if(no1!= no2){
-            while(no2){
-                TViz a1 = buscaAresta(g,no1->id_no,no2->id_no);
-                TViz a2 = buscaAresta(g,no2->id_no,no1->id_no);
-                if(a1 && a2){
-                    //Printa que o nome das duas sendo conexas;
-                }
-            no2 = no2->prox_no;
-            }
-        }
-        no1 = no1->prox_no;
-    }
-
-
-}
 /**            PERGUNTAS PRA FAZER PRA TIA BEBEL <3
 +Perguntar se podem existir nos com valores repetidos em um grafo R: Não
 +Perguntar se eu posso usar estruturas auxiliares para guardar pontes
@@ -190,6 +23,12 @@ A logica:
       Se todos os nos sao alcancaveis a partir dos outros nos, ele nao e orientado.
       Lgo, se existe um no para qual essa sentenca e falsa, o grafo e orientado.
 
+/*Vou alterar um pouco a logica
+
+Se elas forem fortemente conexas, eu so pinto elas da mesma cor,
+ai eu crio uma função imprime que imprime todos os nos que tiverem a mesma cor.
+
+*/
 Implementado mais facilmente usando buscaAresta
 
 */
@@ -239,9 +78,9 @@ int procuraCaminho(TG * g, int id1, int id2){
     if(v->id_viz == id2) return 1;
 
     if(!v->passou){
-    v->passou = 1;
-    resp = procuraCaminho(g,v->id_viz,id2);
-    v->passou = 0;
+      v->passou = 1;
+      resp = procuraCaminho(g,v->id_viz,id2);
+      v->passou = 0;
     }
     v=v->prox_viz;
   }
@@ -271,16 +110,26 @@ int conexo(TG * g){
   resp = 1;
   while(p){
     TNO * q = g->prim;
+
     while(q){
-      if(q->id!=p->id){
-        if(procuraCaminho(g,p->id,q->id) && (!q->cor)){
+      if(q->id_no!=p->id_no){
+
+        if(procuraCaminho(g,p->id_no,q->id_no)){
+          if(q->cor != p->cor) g->numCores--;
           q->cor = p->cor;
         }
-        else if (!procuraCaminho(g,p->id,q->id) && (!q->cor)){
-          g->numCores++;
-          cor=g->numCores;
-          q->cor = cor;
-          pintaSub(g,q->id);
+
+        else if (!procuraCaminho(g,p->id_no,q->id_no)){
+          if(!q->cor){
+            int pintar = verificaCor(g,q->id_no);
+            if(!pintar){
+              g->numCores++;
+              cor=g->numCores;
+              q->cor = cor;
+            }
+            else pintaSub(g,q->id_no);
+
+          }
           resp = 0;
         }
       }
@@ -300,13 +149,25 @@ void pintaSub(TG * g, int no){//pinta toda a componente conexa
   while(a){
     TNO * viz = buscaNo(g,a->id_viz);
     if(!viz->cor) viz->cor = cor;
-    pintaGrafo(g,a->id_viz);
+    pintaSub(g,a->id_viz);
     a=a->prox_viz;
   }
   return;
 }
 
+int verificaCor(TG * g,int id){//verifica se existe algum NO na componente conexa que possua cor
+  if(!g) return 0;
 
+  TNO * no1 = buscaNo(g, id);
+  if(!no1) return 0;
+
+  TNO * no2 = g->prim;
+  while(no2){
+    if(procuraCaminho(g,no1->id,no2->id) && no2->cor) return no2->cor;
+    no2=no2->prox_no;
+  }
+  return 0;
+}
 
 /**TODO
 
@@ -317,41 +178,75 @@ Procura um caminho do no V ate o no W
     se nao, e uma ponte
 Insere a aresta novamente
 */
-int verificaPonte(TG * g, int id1, int id2){
-  if(!g) return;
-
-  TNO * no1 = buscaNo(g, id1);
-  if(!no1) return 0;
-  TNO * no2 = buscaNo(g, id2);
-  if(!no2) return 0;
-  TViz * aresta = buscaAresta(g, id1, id2);
-  if(!aresta) return 0;
-
-  removeAresta(g,id1,id2);
-  removeAresta(g,id2,id1);
-
-  int caminho = procuraCaminho(g,id1,id2);
-
-  if(!caminho) return 0;
-  return 1;
-}
 
 void achaPontes(TG * g){
   if(!g) return 0;
   TNO * p = g->prim;
-  while(p && !p->prim_viz){
+  while(p){
+    while(p && (!p->prim_viz)) p = p->prox_no;
+    if(!p) return;
+    TViz * v = p->prim_viz;
+
+    int primAresta = v->id_viz;
+
+    do{
+      int aux = v->id_viz;
+      removeAresta(g,p->id_no,aux);
+      int caminho = procuraCaminho(g,p->id_no,aux);
+      if(!caminho){printf("%d faz ponte com %d",p->id_no,aux)}
+      insereAresta(g,p->id_no,aux);
+    }while(aux!=primAresta)
     p=p->prox_no;
   }
-  if(!p) return;
-  TViz * a = p->prim_viz;
-  while(a){
-    TNO * v = buscaNo(g,v->id_viz);
-    
-  }
-
 }
 
 /**TODO
-                      Checando componente conexa
+                      Checando componente fortemente conexa
 
 */
+/*Vou alterar um pouco a logica
+
+Se elas forem fortemente conexas, eu so pinto elas da mesma cor,
+ai eu crio uma função imprime que imprime todos os nos que tiverem a mesma cor.
+
+*/
+void fortementeConexa(TG * g){
+  TNO * no1 = g->prim;
+  int corNo1 = no->cor;
+  int totalCores = g->numCores;
+  while(no1){
+    TNO * no2 = g->prim;
+    if(no1!= no2){
+      while(no2){
+        int vTow = procuraCaminho(g, no1->id_no, no2->id_no);
+        int wTov = procuraCaminho(g, no2->id_no, no1->id_no);
+        if(vTow && wTov){
+          no2->cor = corNo1;
+        }
+        else
+        no2 = no2->prox_no;
+      }
+    }
+  no1 = no1->prox_no;
+  }
+}
+TG * criaGrafo(char * nomeArq){
+    FILE * arquivo = fopen(nomeArq,"rt");
+    if(!arquivo) exit(1);
+
+    int numNos,no1,no2,n=0;
+    fscanf(arquivo,"%d",&numNos);
+
+    TG* g = inicializa();
+
+    r = fscanf(arquivo,"%d%d",&no1,&no2);
+    while(n<numNos){
+      insereNo(g,no1);
+      insereAresta(g,no,no2);
+      n++;
+      r = fscanf(arquivo,"%d %d",&n,&i);
+    }
+
+    fclose(arquivo);
+    return g;
+}
