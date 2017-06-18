e73614bb69659f26cc4de4cbc42d2e2e442821f5
Mauricio Milano <mauricio.milano@id.uff.br>
2017-06-18 08:11:59 -0300

    busca nó, insere nó, remove nó, insere aresta, busca aresta, remove aresta e libera feitos

--- grafo.h
+++ grafo.h
@@ -1,31 +1,13 @@
-typedef struct grafo{
-  TNO * prim;
-  int numCores;
-}TG;
+typedef struct grafo{TG;
+typedef struct no TNO;
+typedef struct viz TViz;
 
-typedef struct no{
-  int id_no;
-  int cor;
-  TViz * prim_viz;
-  struc no * prox_no;
-}TNO;
-
-typedef struct viz{
-  int id_viz;
-  int custo;
-  struct viz * prox_viz;
-}TViz;
-
-
-void imprime (TG * g ){
-    TNO * p = g->prim;
-    while(p){
-        printf("%d \n", p->id_no);
-        TViz * v = p->prim_viz;
-        while(v){
-            printf("viz: %d \t custo: %d", v->id_viz, v->custo);
-            v=v->prox_viz;
-        }
-        p=p->prox_no;
-    }
-}
+void imprime (TG * g );
+TNO * buscaNo(TG * g, int elem);
+void removeNO(TG * g, int elem);
+TViz * buscaAresta(TG *g, int no1, int no2);
+void checaOrientação(TG * g);
+int conexo (TG * g);
+void pintaSub(TG *g);
+int verificaPonte(TG * g, int id1, int id2);
+void achaPontes(TG * g);
